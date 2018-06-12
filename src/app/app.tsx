185b8d7513bf607077181ada19f152207101d856
Michal <michal.raczkowski@zalando.de>
2018-06-12 12:30:47 +0200

    remove console.log

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -11,8 +11,6 @@ export interface AppProps {
 export class App extends React.Component<AppProps> {
   public render() {
     const classes = this.props.classes || {};
-    // tslint:disable-next-line:no-console
-    console.log('classes.links: ', classes.links);
 
     return (
       <div className={`${classes.root || defaultClasses.root}`}>
